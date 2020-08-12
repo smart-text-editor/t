6d112654ad8997afd88fd446b42bf451e9a43c64
dev-ste <64751972+dev-ste@users.noreply.github.com>
2020-08-12 12:19:20 -0700

    Uploaded version 1.0

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const cacheName = 1.63;
+self.addEventListener("activate",function(event){
+  event.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+        cacheNames.map(function(cache){
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+self.addEventListener("fetch",function(event){
+  event.respondWith(
+    fetch(event.request).then(function(resolve){
+      const resClone = resolve.clone();
+      caches.open(cacheName).then(function(cache){
+        cache.put(event.request,resClone);
+      });
+      return resolve;
+    })
+    .catch(function(error){
+      caches.match(event.request).then(function(resolve){
+        return resolve;
+      })
+    })
+  );
+});
